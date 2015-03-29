47b9502ed19c6bb2f395517a7340e5dae64eda02
Pericles Theodorou <periclestheo@gmail.com>
2015-03-29 00:55:17 +0000

    Add documentation as to how the request errors can be handled

--- lib/onfido/request_error.rb
+++ lib/onfido/request_error.rb
@@ -1,3 +1,20 @@
+=begin
+The class will handle response failures coming from Onfido
+It's main purpose is to produce meaningful error messages to the user e.g.
+RequestError: Authorization error: please re-check your credentials
+
+Users can also rescue the error and insect its type and affected fields as
+specified in the Onfido documentation e.g.
+
+begin
+  # error being raised
+rescue Onfido::RequestError => e
+  e.type
+  e.fields
+end
+
+=end
+
 module Onfido
   class RequestError < StandardError
     attr_accessor :type, :fields
