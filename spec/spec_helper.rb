c42b8bf74bad2934fac70677926e3944bb8902da
Pericles Theodorou <periclestheo@gmail.com>
2015-03-29 00:16:34 +0000

    Expose rspec dsl globally to use describe instead of Rspec.describe

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,6 +30,7 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.disable_monkey_patching!
   config.warnings = false
+  config.expose_dsl_globally = true
 
   # Seed global randomization in this process using the `--seed` CLI option.
   # Setting this allows you to use `--seed` to deterministically reproduce
