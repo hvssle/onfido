fad5a653f7f6b5af0f62c925bd666b37ccc264aa
Pericles Theodorou <periclestheo@gmail.com>
2015-03-29 03:55:13 +0100

    Create a ResponseHandler to parse the response and handle any errors
    If the user specifies throws_exceptions option to true and an error
    occurs, then an error will be raise. Otherwise the response body will be
    returned whether the response is successful or not.

--- lib/onfido.rb
+++ lib/onfido.rb
@@ -1,6 +1,9 @@
+require 'json'
+
 require 'onfido/version'
 require 'onfido/configuration'
 require 'onfido/request_error'
+require 'onfido/response_handler'
 
 module Onfido
   extend Configuration
---
+++ lib/onfido/response_handler.rb
@@ -0,0 +1,37 @@
+module Onfido
+  class ResponseHandler
+    attr_reader :response
+
+    def initialize(response)
+      @response = response
+    end
+
+    def parse!
+      if Onfido.throws_exceptions && parsed_response["error"]
+        raise request_error
+      else
+        parsed_response
+      end
+    end
+
+    private
+
+    def parsed_response
+      @parsed_response ||=
+        begin
+          JSON.parse(response)
+        rescue JSON::ParserError
+          {
+            'error' => {"message" => "Unparseable response: #{response}"}
+          }
+        end
+    end
+
+    def request_error
+      RequestError.new(parsed_response['error']['message']).tap do |error|
+        error.type = parsed_response['error']["type"]
+        error.fields = parsed_response['error']["fields"]
+      end
+    end
+  end
+end
