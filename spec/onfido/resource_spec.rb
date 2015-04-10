30eaeaeb7b419e523bede8ab5b2a8dde88e68db0
Pericles Theodorou <periclestheo@gmail.com>
2015-04-10 10:59:45 +0100

    Create a generic resource class for building up requests to the API

---
+++ spec/onfido/resource_spec.rb
@@ -0,0 +1,58 @@
+describe Onfido::Resource do
+  subject(:resource) { described_class.new }
+  let(:endpoint) { 'https://onfido.com/api/v1/' }
+  let(:path) { 'addresses/pick' }
+  let(:api_key) { 'some_key' }
+
+  before do
+    allow(Onfido).to receive(:endpoint).and_return(endpoint)
+    allow(Onfido).to receive(:api_key).and_return(api_key)
+  end
+
+  describe '#url_for' do
+    it 'composes the full api url' do
+      expect(resource.url_for(path)).to eq(endpoint + path)
+    end
+  end
+
+  describe '#method_missing' do
+    %i(put delete patch).each do |method|
+      context "for unsupported HTTP method: #{method}" do
+        it 'raises an error' do
+          expect {
+            resource.public_send(method)
+          }.to raise_error(NoMethodError, "undefined method '#{method}' for #{resource.class}")
+        end
+      end
+    end
+
+    %i(get post).each do |method|
+      context "for supported HTTP method: #{method}" do
+        let(:url) { endpoint + path }
+        let(:payload) { {postcode: 'SE1 4NG'} }
+        let(:response) do
+          {
+            'addresses' => [
+              {
+                'street' => 'Main Street',
+                'town' => 'London',
+                'postcode' => 'SW4 6EH',
+                'country' => 'GBR'
+              }
+            ]
+          }
+        end
+
+        before do
+          expect(RestClient::Request).to receive(:execute)
+            .with(url: url, payload: payload, method: method, headers: resource.send(:headers))
+            .and_return(response)
+        end
+
+        it 'makes a request to an endpoint' do
+          expect(resource.public_send(method, {url: url, payload: payload})).to eq(response)
+        end
+      end
+    end
+  end
+end
