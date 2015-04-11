a16592b3a6d6f291850d84ed0f7fbeaf148bb255
Pericles Theodorou <periclestheo@gmail.com>
2015-04-11 16:24:41 +0100

    Check Onfido::RequestError has the right values when raised

--- spec/onfido/request_error_spec.rb
+++ spec/onfido/request_error_spec.rb
@@ -3,6 +3,7 @@ describe Onfido::RequestError do
     described_class.new(failed_response['error']['message']).tap do |e|
       e.type = failed_response['error']['type']
       e.fields = failed_response['error']['fields']
+      e.response_code = 401
     end
   end
 
@@ -21,6 +22,18 @@ describe Onfido::RequestError do
   context 'when there is a request error' do
     it 'returns the right message' do
       expect { raise error }.to raise_error('Authorization error: please re-check your credentials')
+    end
+
+    it 'has the right error type' do
+      expect(error.type).to eq('authorization_error')
+    end
+
+    it 'has the right affected fields' do
+      expect(error.fields).to eq({'name' => {'messages' => ['cannot be blank']} })
+    end
+
+    it 'has the right response code' do
+      expect(error.response_code).to eq(401)
     end
   end
 end
