8f257befee3dbf09392228427980f7afb9598fb8
Daniel José Domingos Serrano <danieljdserrano@gmail.com>
2016-08-01 11:24:11 +0100

    Add tests for dynamic API keys
    Relates to issue #16 raised by @soolaimon, in repo hvssle/onfido.
    
    In the two specs added we test both cases:
      * Using a specific API key, in which the header should reflect that
      value
      * Passing a `nil` value for the API key, in which the header should
      not use `nil`, but instead use the globally configured Onfido API key
    
    Note: The case of not passing any argument to the initializer was
    already being implicitly covered by other tests.
    
    ISSUE [#15]
    PR [#16]

--- spec/onfido/resource_spec.rb
+++ spec/onfido/resource_spec.rb
@@ -1,8 +1,24 @@
 describe Onfido::Resource do
   subject(:resource) { described_class.new }
+
   let(:endpoint) { 'https://api.onfido.com/v2/' }
-  let(:path) { 'addresses/pick' }
-  let(:api_key) { 'some_key' }
+  let(:path)     { 'addresses/pick' }
+  let(:url)      { endpoint + path }
+  let(:payload)  { { postcode: 'SE1 4NG' } }
+  let(:api_key)  { 'some_key' }
+
+  let(:response) do
+    {
+      'addresses' => [
+        {
+          'street' => 'Main Street',
+          'town' => 'London',
+          'postcode' => 'SW4 6EH',
+          'country' => 'GBR'
+        }
+      ]
+    }
+  end
 
   before { allow(Onfido).to receive(:endpoint).and_return(endpoint) }
   before { allow(Onfido).to receive(:api_key).and_return(api_key) }
@@ -25,24 +41,54 @@ describe Onfido::Resource do
     end
   end
 
+  describe "API key" do
+    subject(:resource) { described_class.new(specific_api_key) }
+
+    before do
+      expect(RestClient::Request).to receive(:execute).
+        with(
+          url: url,
+          payload: Rack::Utils.build_query(payload),
+          method: :get,
+          headers: resource.send(:headers),
+          open_timeout: 30,
+          timeout: 80
+      ).and_call_original
+
+      WebMock.stub_request(:get, url).
+        to_return(body: response.to_json, status: 200)
+    end
+
+    context "when using a specific key" do
+      let(:specific_api_key) { "specific_key" }
+
+      it "uses that key when making the request" do
+        resource.get(url: url, payload: payload)
+
+        expect(WebMock).to have_requested(:get, url).with(headers: {
+          'Authorization' => "Token token=#{specific_api_key}",
+          'Accept' => "application/json"
+        })
+      end
+    end
+
+    context "when not using a specific key" do
+      let(:specific_api_key) { nil }
+
+      it "uses the general config key when making the request" do
+        resource.get(url: url, payload: payload)
+
+        expect(WebMock).to have_requested(:get, url).with(headers: {
+          'Authorization' => "Token token=#{api_key}",
+          'Accept' => "application/json"
+        })
+      end
+    end
+  end
+
   describe "valid http methods" do
     %i(get post).each do |method|
       context "for supported HTTP method: #{method}" do
-        let(:url) { endpoint + path }
-        let(:payload) { { postcode: 'SE1 4NG' } }
-        let(:response) do
-          {
-            'addresses' => [
-              {
-                'street' => 'Main Street',
-                'town' => 'London',
-                'postcode' => 'SW4 6EH',
-                'country' => 'GBR'
-              }
-            ]
-          }
-        end
-
         before do
           expect(RestClient::Request).to receive(:execute).
             with(
