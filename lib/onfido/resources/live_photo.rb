e6ef1e798cc58ab948779a2fceca830d870e793c
Henry Heath <henry.heath@onfido.com>
2016-09-02 14:45:23 +0100

    Unnest the live_photos endpoint

--- lib/onfido/resources/live_photo.rb
+++ lib/onfido/resources/live_photo.rb
@@ -3,9 +3,10 @@ module Onfido
     # with open-uri the file can be a link or an actual file
 
     def create(applicant_id, payload)
+      payload[:applicant_id] = applicant_id
       payload[:file] = open(payload.fetch(:file), 'r')
       post(
-        url: url_for("applicants/#{applicant_id}/live_photos"),
+        url: url_for("/live_photos"),
         payload: payload
       )
     end
