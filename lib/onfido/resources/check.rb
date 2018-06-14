767586ef929ad553b3446d910cf7b771d71604b1
Dylan Pierce <dylanpierce914@gmail.com>
2018-06-14 15:07:05 -0600

    Adding Check#find_by_url which allows a partial or full url to the check resource

--- lib/onfido/resources/check.rb
+++ lib/onfido/resources/check.rb
@@ -13,6 +13,12 @@ module Onfido
                        "#{querystring}"))
     end
 
+    def find_by_url(url, expand: nil)
+      url_path = url.sub(/^https\:\/\/api\.onfido\.com\/v2\//,'')
+      querystring = "&expand=#{expand}" if expand
+      get(url: url_for("#{url_path}?#{querystring}")) 
+    end
+
     def all(applicant_id, page: 1, per_page: 20, expand: nil)
       querystring = "page=#{page}&per_page=#{per_page}"
       querystring += "&expand=#{expand}" if expand
--- spec/integrations/check_spec.rb
+++ spec/integrations/check_spec.rb
@@ -30,6 +30,48 @@ describe Onfido::Check do
     end
   end
 
+  describe '#find_by_url' do
+    let(:check_id) { '8546921-123123-123123' }
+
+    context 'partial url' do
+      let(:url) { "applicants/#{applicant_id}/checks/#{check_id}" }
+      
+      it 'returns an existing check for the applicant with the partial url' do
+        response = check.find_by_url(url)
+        expect(response['id']).to eq(check_id)
+      end
+
+      it "returns unexpanded reports" do
+        response = check.find_by_url(url)
+        expect(response['reports'].first).to be_a(String)
+      end
+
+      it 'allows you to expand the reports' do
+        response = check.find_by_url(url, expand: "reports")
+        expect(response['reports'].first).to be_a(Hash)
+      end
+    end
+
+    context 'full url' do
+      let(:url) { "https://api.onfido.com/v2/applicants/#{applicant_id}/checks/#{check_id}" }
+
+      it 'returns an existing check for the applicant with the partial url' do
+        response = check.find_by_url(url)
+        expect(response['id']).to eq(check_id)
+      end
+
+      it "returns unexpanded reports" do
+        response = check.find_by_url(url)
+        expect(response['reports'].first).to be_a(String)
+      end
+
+      it 'allows you to expand the reports' do
+        response = check.find_by_url(url, expand: "reports")
+        expect(response['reports'].first).to be_a(Hash)
+      end
+    end
+  end
+
   describe '#all' do
     let(:check_id) { '8546921-123123-123123' }
 
